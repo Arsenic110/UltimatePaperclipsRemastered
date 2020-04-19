3f0ca2524abd5006ffa461ec67ea9d91173ee69c
deadlybite110 <kubarodak2003@outlook.com>
2020-04-19 15:39:39 +0100

    added basic file tree and some style and broken js

---
+++ js/main.js
@@ -0,0 +1,37 @@
+var docs = document.getElementsByClassName("readout");
+var iteration = 0;
+
+
+for(var i = 0; i < docs.length; i++)
+{
+    write(docs[i], 150);
+}
+
+function write(s, d)
+{
+    let h;
+    if(iteration == 0)
+    {
+        h = s.innerHTML;
+        s.innerHTML = "";
+        console.log("wiped");
+    }
+    else
+    {
+        s.innerHTML += h[iteration];
+        console.log(h[iteration]);
+        iteration++;
+        sleep(d);   
+    }
+}
+
+function sleep(ms)
+{
+    const date = Date.now();
+    let currentDate = null;
+    do
+    {
+        currentDate = Date.now();
+    }
+    while(currentDate - date < ms);
+}
